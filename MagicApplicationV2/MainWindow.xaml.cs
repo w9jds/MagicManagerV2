ca8df9b1e72b0820cc9c2e40a8e9f52b1bfcfb17
Jeremy Shore <xboxmusicn3rd@gmail.com>
2013-03-09 09:40:52 -0600

    Search Function now works. VERY POWERFUL! MWAHAHA

--- MagicApplicationV2/MainWindow.xaml.cs
+++ MagicApplicationV2/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using MagicApplicationV2.Windows;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.OleDb;
 using System.Linq;
@@ -24,7 +25,7 @@ namespace MagicApplicationV2
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<CardListData> Cards = new List<CardListData>();
+        private ObservableCollection<CardListData> Cards = new ObservableCollection<CardListData>();
 
         public MainWindow()
         {
@@ -33,7 +34,7 @@ namespace MagicApplicationV2
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         { 
-           await GetCards();
+           GetCards();
         }
 
         private void CardsList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -68,7 +69,7 @@ namespace MagicApplicationV2
                         Toughness = CardDS.Tables[0].Rows[i]["Toughness"].ToString()
                     });
 
-                CardsList.ItemsSource = Cards;
+                this.CardsList.ItemsSource = Cards;
             }
             catch (Exception) {
                 PopupWin ErrorWin = new PopupWin();
@@ -108,7 +109,65 @@ namespace MagicApplicationV2
         {
             PopupWin SettingsWin = parent as PopupWin;
             SettingsWin.Close();
-            await GetCards();
+            GetCards();
+        }
+
+        private async void Search_Click(object sender, RoutedEventArgs e)
+        {
+            FilterDB();
+        }
+
+        private async Task FilterDB()
+        {
+            List<string> Filter = new List<string>();
+
+            if (CardNameCheck.IsChecked == true && CardNameBox.Text != "")
+                Filter.Add("%" + CardNameBox.Text + "%");
+            else
+                Filter.Add("%");
+            
+            if (CardExpanCheck.IsChecked == true && CardExpanBox.Text != "")
+                Filter.Add("%" + CardExpanBox.Text + "%");
+            else
+                Filter.Add("%");
+
+
+            try
+            {
+                OleDbConnection DBCon = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + Properties.Settings.Default.DatabaseLocation);
+                await DBCon.OpenAsync();
+                
+                OleDbDataAdapter CardDA = new OleDbDataAdapter("SELECT * FROM Cards WHERE Name LIKE '" + Filter[0] + "' and Expansion LIKE '" + Filter[1] + "'", DBCon);
+                DataSet CardDS = new DataSet();
+                CardDA.Fill(CardDS);
+                DBCon.Close();
+
+                Cards.Clear();
+
+                for (int i = 0; i < CardDS.Tables[0].Rows.Count; i++)
+                        Cards.Add(new CardListData
+                        {
+                            MultiverseID = CardDS.Tables[0].Rows[i]["MultiverseID"].ToString(),
+                            CardName = CardDS.Tables[0].Rows[i]["Name"].ToString(),
+                            CardExpansion = CardDS.Tables[0].Rows[i]["Expansion"].ToString(),
+                            CardImg = CardDS.Tables[0].Rows[i]["ImgURL"].ToString(),
+                            Rarity = CardDS.Tables[0].Rows[i]["Rarity"].ToString(),
+                            ConvMana = CardDS.Tables[0].Rows[i]["ConvManaCost"].ToString(),
+                            Type = CardDS.Tables[0].Rows[i]["Type"].ToString(),
+                            Power = CardDS.Tables[0].Rows[i]["Power"].ToString(),
+                            Toughness = CardDS.Tables[0].Rows[i]["Toughness"].ToString()
+                        });
+            }
+            catch (Exception) {
+                PopupWin ErrorWin = new PopupWin();
+                Error ErrorControl = new Error(ErrorWin);
+                ErrorWin.ControlGrid.Children.Add(ErrorControl);
+                ErrorControl.ErrorText.Text = "There was a problem connection to the cards database. Please go to File > Settings and make sure the path is correct.";
+                ErrorWin.Show();
+                ErrorControl.CloseWin += new Error.ErrorControlDelegate(CloseWindow);
+            };
+
+
         }
     }
 }
